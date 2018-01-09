4e3b8273220f5ce055f45d0dbaf5088065b669bb
innay96 <33022672+innay96@users.noreply.github.com>
2018-01-09 10:31:53 +0200

    Update filters.java

--- version14/filters.java
+++ version14/filters.java
@@ -9,12 +9,33 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
+import java.awt.Font;
 
 public class filters extends JFrame {
 
+	private boolean ok=false;
 	private JPanel contentPane;
+	private String csvPath;
+	private String wigglePath;
+
+	public String getCsvPath() {
+		return csvPath;
+	}
+
+	public void setCsvPath(String dataPath) {
+		this.csvPath = dataPath;
+	}
+
+	public String getWigPath() {
+		return wigglePath;
+	}
+
+	public void setWigPath(String wigPath) {
+		this.wigglePath = wigPath;
+	}
 
 	/**
 	 * Launch the application.
@@ -37,43 +58,63 @@ public class filters extends JFrame {
 	 */
 	public filters() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 921, 685);
+		setBounds(100, 100, 1041, 335);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblPleaseChooseWhich = new JLabel("please choose which filter you want to use.");
-		lblPleaseChooseWhich.setBounds(130, 54, 641, 20);
+		lblPleaseChooseWhich.setFont(new Font("Trebuchet MS", Font.PLAIN, 22));
+		lblPleaseChooseWhich.setBounds(49, 39, 641, 20);
 		contentPane.add(lblPleaseChooseWhich);
-		
+
 		JLabel lblIfYouWant = new JLabel("if you want to filter by more than one filter, please choose the option \"more than one\"");
-		lblIfYouWant.setBounds(130, 85, 633, 20);
+		lblIfYouWant.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblIfYouWant.setBounds(49, 75, 878, 53);
 		contentPane.add(lblIfYouWant);
-		
+
 		JButton btnRadious = new JButton("Radious");
 		btnRadious.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
+
+
 			}
 		});
 		btnRadious.setBounds(49, 181, 115, 29);
 		contentPane.add(btnRadious);
-		
+
 		JButton btnTime = new JButton("Time");
+		btnTime.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				timeFil tim= new timeFil();
+				tim.setCsvPath(csvPath);
+				tim.setWigPath(wigglePath);
+				contentPane.hide();
+				tim.setVisible(true);
+			}
+		});
 		btnTime.setBounds(334, 181, 115, 29);
 		contentPane.add(btnTime);
-		
+
 		JButton btnLocation = new JButton("Location");
 		btnLocation.setBounds(476, 181, 115, 29);
 		contentPane.add(btnLocation);
-		
+
 		JButton btnMoreThanOne = new JButton("more than one");
 		btnMoreThanOne.setBounds(622, 181, 196, 29);
 		contentPane.add(btnMoreThanOne);
-		
+
 		JButton btnId = new JButton("ID");
+		btnId.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				idFil idf= new idFil();
+				idf.setCsvPath(csvPath);
+				idf.setWigPath(wigglePath);
+				contentPane.hide();
+				idf.setVisible(true);
+			}
+		});
 		btnId.setBounds(191, 181, 115, 29);
 		contentPane.add(btnId);
 	}
