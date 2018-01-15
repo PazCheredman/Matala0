de39b3ef148ca07190251806f0e01608e360e40f
innay96 <33022672+innay96@users.noreply.github.com>
2018-01-15 23:02:08 +0200

    Update outputTable.java

--- version14/outputTable.java
+++ version14/outputTable.java
@@ -116,14 +116,6 @@ public class outputTable {
 		/**
 		 * write header line
 		 */
-		/*writer.write("Time" + "," + "ID" + "," + "Lat" + "," + "Lon" + "," + "Alt" + "," + "#WiFi networks" + ","
-				+ "SSID1" + "," + "MAC1" + "," + "Frequncy1" + "," + "Signal1" + "," + "SSID2" + "," + "MAC2" + ","
-				+ "Frequncy2" + "," + "Signal2" + "," + "SSID3" + "," + "MAC3" + "," + "Frequncy3" + "," + "Signal3"
-				+ "," + "SSID4" + "," + "MAC4" + "," + "Frequncy4" + "," + "Signal4" + "," + "SSID5" + "," + "MAC5"
-				+ "," + "Frequncy5" + "," + "Signal5" + "," + "SSID6" + "," + "MAC6" + "," + "Frequncy6" + ","
-				+ "Signal6" + "," + "SSID7" + "," + "MAC7" + "," + "Frequncy7" + "," + "Signal7" + "," + "SSID8" + ","
-				+ "MAC8" + "," + "Frequncy8" + "," + "Signal8" + "," + "SSID9" + "," + "MAC9" + "," + "Frequncy9" + ","
-				+ "Signal9" + "," + "SSID10" + "," + "MAC10" + "," + "Frequncy10" + "," + "Signal10");*/
 		writer.write("Time,ID,Lat,Lon,Alt,#WiFi networks,SSID1,MAC1,Frequncy1,Signal1"+
 				",SSID2,MAC2,Frequncy2,Signal2,SSID3,MAC3,Frequncy3,Signal3"+
 				",SSID4,MAC4,Frequncy4,Signal4,SSID5,MAC5,Frequncy5,Signal5"+
@@ -131,7 +123,6 @@ public class outputTable {
 				",SSID8,MAC8,Frequncy8,Signal8,SSID9,MAC9,Frequncy9,Signal9"+
 				",SSID10,MAC10,Frequncy10,Signal10");
 		writer.write("\n");
-		
 		
 		for (int i = 0; i < ls.size(); i++) {
 			outputRow row = ls.get(i);
